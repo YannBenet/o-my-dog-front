e99c8384e27654d2d5acaf7bc082d09eb01585a8
Orkblan <bossolay@hotmail.fr>
2024-07-01 17:27:47 +0200

    feat: :lipstick: add inscription and cennexion page

--- src/components/Page/Inscription.tsx
+++ src/components/Page/Inscription.tsx
@@ -1,7 +1,48 @@
 import './Inscription.scss';
 
 function Inscription() {
-  return <h1>Inscription</h1>;
+  return (
+    <section className="container-inscription">
+      <h2>Inscription</h2>
+      <section>
+        <form action="submit" className="container-inscription-form">
+          <input
+            type="text"
+            placeholder="ici"
+            className="container-inscription-form-input"
+          />
+          <input
+            type="text"
+            placeholder="ici"
+            className="container-inscription-form-input"
+          />
+          <input
+            type="text"
+            placeholder="ici"
+            className="container-inscription-form-input"
+          />
+          <input
+            type="text"
+            placeholder="ici"
+            className="container-inscription-form-input"
+          />
+          <input
+            type="text"
+            placeholder="ici"
+            className="container-inscription-form-input"
+          />
+          <input
+            type="text"
+            placeholder="ici"
+            className="container-inscription-form-input"
+          />
+          <button type="submit" className="container-inscription-form-button">
+            Validé
+          </button>
+        </form>
+      </section>
+    </section>
+  );
 }
 
 export default Inscription;
