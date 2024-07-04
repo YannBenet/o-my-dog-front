402eba85d934fe6085aac5a5f69e8110dff3090f
Username Github <zhb@hotmail.fr>
2024-07-04 22:50:05 +0200

    feat: calendar modification

--- src/components/Page/Availability.tsx
+++ src/components/Page/Availability.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
-import '../PageStyle/Availability.scss';
+import { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import '../PageStyle/Availability.scss';
 
 type ValuePiece = Date | null;
 
@@ -8,9 +9,22 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 function Availability() {
   const [value, setValue] = useState<Value>(new Date());
+
+  useEffect(()=> {
+    setValue([
+      new Date("2024-07-11T22:00:00.000Z"),
+      new Date("2024-07-19T21:59:59.999Z")
+    ])
+  },[])
+
+  console.log(value);
+
   return (
     <div>
       <Calendar selectRange onChange={setValue} value={value} />
+      <p>Date value 1: {Array.isArray(value) ? value[0].toDateString() : value.toDateString()}</p>
+      <p>Date value 2: {Array.isArray(value) && value[1] ? value[1].toDateString() : 'N/A'}</p>
+    
     </div>
   );
 }
