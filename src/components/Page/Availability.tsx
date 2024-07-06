db7e16bfd22b138b8acc366426377184ffeb4a51
Orkblan <bossolay@hotmail.fr>
2024-07-06 21:26:05 +0200

    fix: CRLF => LF

--- src/components/Page/Availability.tsx
+++ src/components/Page/Availability.tsx
@@ -10,21 +10,26 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 function Availability() {
   const [value, setValue] = useState<Value>(new Date());
 
-  useEffect(()=> {
+  useEffect(() => {
     setValue([
-      new Date("2024-07-11T22:00:00.000Z"),
-      new Date("2024-07-19T21:59:59.999Z")
-    ])
-  },[])
+      new Date('2024-07-11T22:00:00.000Z'),
+      new Date('2024-07-19T21:59:59.999Z'),
+    ]);
+  }, []);
 
   console.log(value);
 
   return (
     <div>
       <Calendar selectRange onChange={setValue} value={value} />
-      <p>Date value 1: {Array.isArray(value) ? value[0].toDateString() : value.toDateString()}</p>
-      <p>Date value 2: {Array.isArray(value) && value[1] ? value[1].toDateString() : 'N/A'}</p>
-    
+      <p>
+        Date value 1:{' '}
+        {Array.isArray(value) ? value[0].toDateString() : value.toDateString()}
+      </p>
+      <p>
+        Date value 2:{' '}
+        {Array.isArray(value) && value[1] ? value[1].toDateString() : 'N/A'}
+      </p>
     </div>
   );
 }
